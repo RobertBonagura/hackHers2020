4fed2c8e3116850aa84fd8689bd4337d8d29a97f
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2020-02-22 17:21:44 -0500

    Add files via upload

--- Load_Model.py
+++ Load_Model.py
@@ -1,6 +1,12 @@
 import tensorflow
 import numpy
+import os
 from tensorflow.keras.models import load_model
+
+#STRING INPUT
+string_data = "1000000,2,6,3,79,8,8,8,8,8,8,964511,983931,1664089,891586,927171,961664,873552,1684259,896040,621000,426529,528666"
+data = string_data.split(",")
+
 
 INPUT_DIMENSION = 23
 MAX_VALUE = [1000000, 2, 6, 3, 79, 8, 8, 8, 8, 8, 8, 964511, 983931, 1664089,
@@ -9,15 +15,16 @@ MIN_VALUE = [10000, 1, 0, 0, 21, -2, -2, -2, -2, -2, -2, -165580, -
              69777, -157264, -170000, -81334, -339603, 0, 0, 0, 0, 0, 0]
 
 
-x_in = numpy.empty((1, INPUT_DIMENSION), dtype = numpy.float)
+x_in = numpy.empty((1, INPUT_DIMENSION), dtype=numpy.float)
 
 
 feature = 0
 while feature < INPUT_DIMENSION:
+    x_in[0, feature] = float(data[feature])
     x_in[0, feature] = (
         x_in[0, feature] - MIN_VALUE[feature]) / (MAX_VALUE[feature]-MIN_VALUE[feature])
     feature += 1
 
-
 model = load_model("fiserv_model.h5")
-print(model.predict(x_in))
+print(model.predict(
+    [x_in[:, 0:5], x_in[:, 5:11], x_in[:, 11:17], x_in[:, 17:23]]))
