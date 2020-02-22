7e0f68ac85f0780eec32016da47368b9f6b3fa9f
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2020-02-22 18:03:00 -0500

    Add files via upload

--- Load_Model.py
+++ Load_Model.py
@@ -3,10 +3,9 @@ import numpy
 import os
 from tensorflow.keras.models import load_model
 
-#STRING INPUT
-string_data = "1000000,2,6,3,79,8,8,8,8,8,8,964511,983931,1664089,891586,927171,961664,873552,1684259,896040,621000,426529,528666"
-data = string_data.split(",")
+string_data = "HERE IS YOUR INPUT"
 
+array_data = []
 
 INPUT_DIMENSION = 23
 MAX_VALUE = [1000000, 2, 6, 3, 79, 8, 8, 8, 8, 8, 8, 964511, 983931, 1664089,
@@ -14,9 +13,11 @@ MAX_VALUE = [1000000, 2, 6, 3, 79, 8, 8, 8, 8, 8, 8, 964511, 983931, 1664089,
 MIN_VALUE = [10000, 1, 0, 0, 21, -2, -2, -2, -2, -2, -2, -165580, -
              69777, -157264, -170000, -81334, -339603, 0, 0, 0, 0, 0, 0]
 
-
+# An empty dimension
 x_in = numpy.empty((1, INPUT_DIMENSION), dtype=numpy.float)
 
+for index in range(23):
+    x_in[0, index] = array_data[index]
 
 feature = 0
 while feature < INPUT_DIMENSION:
@@ -24,6 +25,7 @@ while feature < INPUT_DIMENSION:
     x_in[0, feature] = (
         x_in[0, feature] - MIN_VALUE[feature]) / (MAX_VALUE[feature]-MIN_VALUE[feature])
     feature += 1
+
 
 model = load_model("fiserv_model.h5")
 print(model.predict(
