95739219fd31b98b4a83d711b4a4d424588d56c2
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2020-02-22 18:08:56 -0500

    Add files via upload

--- Load_Model.py
+++ Load_Model.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model
 
 string_data = "HERE IS YOUR INPUT"
 
-array_data = []
+array_data = [1000000, 2, 6, 3, 79, 8, 8, 8, 8, 8, 8, 964511, 983931, 1664089,
+             891586, 927171, 961664, 873552, 1684259, 896040, 621000, 426529, 528666]
 
 INPUT_DIMENSION = 23
 MAX_VALUE = [1000000, 2, 6, 3, 79, 8, 8, 8, 8, 8, 8, 964511, 983931, 1664089,
@@ -16,12 +17,9 @@ MIN_VALUE = [10000, 1, 0, 0, 21, -2, -2, -2, -2, -2, -2, -165580, -
 # An empty dimension
 x_in = numpy.empty((1, INPUT_DIMENSION), dtype=numpy.float)
 
-for index in range(23):
-    x_in[0, index] = array_data[index]
-
 feature = 0
 while feature < INPUT_DIMENSION:
-    x_in[0, feature] = float(data[feature])
+    x_in[0, feature] = float(array_data[feature])
     x_in[0, feature] = (
         x_in[0, feature] - MIN_VALUE[feature]) / (MAX_VALUE[feature]-MIN_VALUE[feature])
     feature += 1
